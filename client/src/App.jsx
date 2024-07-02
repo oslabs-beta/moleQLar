fbb61f0043a15816d3decdf71da1855f2636041f
JonathanGhebrial <jony@ghebrial.com>
2024-07-02 14:31:50 -0700

    add team and about page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,24 @@
-import React from "react";
-import MainPage from "./components/MainPage.jsx";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import MainPage from './components/MainPage.jsx';
 import Navbar from './components/NavBar.jsx';
-import './components/Styles/styles.css'
-const App = () =>{
-    return(
-        <div className="App">
-       <Navbar/>
-       <MainPage/>
-        </div>
-    )
-}
+import Team from './components/TeamPage.jsx';
+import About from './components/About.jsx';
+import './components/Styles/styles.css';
+import './components/Styles/team.css';
+import './components/Styles/about.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/team" element={<Team />} />
+        <Route path="/" element={<MainPage />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
+  );
+};
 
 export default App;
