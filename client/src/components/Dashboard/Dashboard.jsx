a1d58ec6095677d4027d18c8dbc485638a60a890
JonathanGhebrial <jony@ghebrial.com>
2024-07-08 13:13:20 -0700

    add dashboard comp

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -1,12 +1,28 @@
 import React, { useContext, useState } from 'react';
-import UploadSqlSchema from "../UploadSqlSchema/UploadSqlSchema";
 import AuthorizedNavbar from '../AuthorizedNavbar/AuthorizedNavbar';
+import addGraph from '../../assets/logos/addGraph.png';
+import { useNavigate } from 'react-router-dom';
+import './dashboard.scss';
 
 function Dashboard() {
+  const navigate = useNavigate();
+
+  const navigateToGraph = () =>{
+    navigate('/graph')
+  }
+
   return (
     <>
-      <AuthorizedNavbar/>
-      <UploadSqlSchema />
+      <AuthorizedNavbar />
+      <div className="dashboard">
+        <h1>Your Saved Graphs</h1>
+        <div className="add-graph-section">
+          <button className="add-graph-button" onClick={navigateToGraph}>
+            <img src={addGraph} alt="Add Graph" />
+          </button>
+          <p>Click here to create a new project</p>
+        </div>
+      </div>
     </>
   );
 }
