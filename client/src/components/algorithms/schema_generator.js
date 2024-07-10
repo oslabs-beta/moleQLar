f1b47f89ccc7201d8f3cc144bc103e693a6a1074
DanHudgens <danw.hudgens@gmail.com>
2024-07-10 08:49:17 -0700

    add resolver generator and modify schema generator

--- client/src/components/algorithms/schema_generator.js
+++ client/src/components/algorithms/schema_generator.js
@@ -1,5 +1,5 @@
 export function schemaGenerator(nodes, edges) {
-  //create aux objects to hold node relationships for O(1) lookup
+  //create aux objects to hold node relationships
   const oneToManyRelationships = {};
   const manyToOneRelationships = {};
 
@@ -24,13 +24,21 @@ export function schemaGenerator(nodes, edges) {
 
   //for each node in nodes argument
   nodes.forEach((node) => {
+    //if plural/singular versions of id are the same, add 'All' to plural version
+    const pluralIsSingular =
+      pluralize(node.id) === pluralize.singular(node.id) ? '_all' : '';
     //query for all elements in type (plural form)
-    query_string += `    ${pluralize(node.id)}: [${pluralize
+    query_string += `    ${pluralize(node.id).replace(
+      /^./,
+      node.id[0].toLowerCase()
+    )}${pluralIsSingular}: [${pluralize
       .singular(node.id)
       .replace(/^./, node.id[0].toUpperCase())}]\n`;
 
     //query for one element in type (by ID - singular form)
-    query_string += `    ${pluralize.singular(node.id)}(id: ID!): ${pluralize
+    query_string += `    ${pluralize
+      .singular(node.id)
+      .replace(/^./, node.id[0].toLowerCase())}(_id: ID!): ${pluralize
       .singular(node.id)
       .replace(/^./, node.id[0].toUpperCase())}\n`;
 
@@ -39,11 +47,11 @@ export function schemaGenerator(nodes, edges) {
       .singular(node.id)
       .replace(/^./, node.id[0].toUpperCase())} {\n`;
     //add id property to type
-    gql_schema += '    id: ID!\n';
+    gql_schema += '    _id: ID!\n';
 
     //add associated columns to GraphQL type
     for (let i = 1; i < node.data.columns.length; i++) {
-      //check if column is required and add column to type
+      //check if field is required
       const required = node.data.columns[i].required ? '!' : '';
       gql_schema += `    ${node.data.columns[i].name}: ${node.data.columns[i].type}${required}\n`;
     }
@@ -73,4 +81,4 @@ export function schemaGenerator(nodes, edges) {
 
   //return final schema
   return gql_schema + query_string;
-}
+};
--- client/src/contexts/AuthContext.js
+++ client/src/contexts/AuthContext.js
@@ -1,5 +1,5 @@
-import React, { createContext, useState, useEffect, useContext } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { createContext, useState, useEffect, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 export const AuthContext = createContext();
@@ -8,34 +8,37 @@ export const AuthProvider = ({ children }) => {
   const navigate = useNavigate();
   const [authState, setAuthState] = useState({
     isAuth: false,
-    username: "",
+    username: '',
   });
 
   useEffect(() => {
     async function verifyUser() {
       // check local storage
-      const storedUsername = localStorage.getItem("username");
-      const token = localStorage.getItem("token");
+      const storedUsername = localStorage.getItem('username');
+      const token = localStorage.getItem('token');
       // verify user token
       let config = {
-        headers: { 'authorization': `${token}` }
-      }
+        headers: { authorization: `${token}` },
+      };
       try {
-        const response = await axios.post('/api/auth/protected', {}, config)
+        const response = await axios.post('/api/auth/protected', {}, config);
         if (response.status !== 200) {
           console.log('response.message:', response.message);
-          console.log('Unable to verify user. Reponse status: ', response.status);
+          console.log(
+            'Unable to verify user. Reponse status: ',
+            response.status
+          );
           setAuthState({
             isAuth: false,
-            username: "",
+            username: '',
           });
           // return;
           return navigate('/login');
         } else {
-          // successful login 
+          // successful login
           const data = response.data;
-          localStorage.setItem("username", data.username);
-          localStorage.setItem("token", response.headers['authorization']);
+          localStorage.setItem('username', data.username);
+          localStorage.setItem('token', response.headers['authorization']);
           setAuthState({
             isAuth: true,
             username: data.username,
@@ -55,11 +58,11 @@ export const AuthProvider = ({ children }) => {
           console.log('Error message:', err.message);
         }
         // return navigate('/login');
-        return;  // success
+        return; // success
       }
     }
     verifyUser();
-    navigate('/dashboard');
+    //navigate('/dashboard');
   }, []);
 
   const signup = async (formData) => {
@@ -74,24 +77,30 @@ export const AuthProvider = ({ children }) => {
       // success
       const data = response.data;
       console.log('data:', data);
-      console.log("data.username:", data.username);
+      console.log('data.username:', data.username);
       if (data.username) {
         setAuthState({
           isAuth: true,
           username: data.username,
           token: data.token,
-        })
+        });
         console.log('signup - setting username/token');
-        localStorage.setItem("username", data.username);
-        localStorage.setItem("token", response.headers['authorization']);
+        localStorage.setItem('username', data.username);
+        localStorage.setItem('token', response.headers['authorization']);
         return; // success
       }
     } catch (err) {
       if (err.response) {
         // The request was made and the server responded with a status code
         // that falls out of the range of 2xx
-        console.log('Failed to sign up. Error response data:', err.response.data);
-        console.log('Failed to sign up. Error response status:', err.response.status);
+        console.log(
+          'Failed to sign up. Error response data:',
+          err.response.data
+        );
+        console.log(
+          'Failed to sign up. Error response status:',
+          err.response.status
+        );
       } else if (err.request) {
         // if request was made, but no response received
         console.log('Error request:', err.request);
@@ -100,18 +109,21 @@ export const AuthProvider = ({ children }) => {
         console.log('Error message:', err.message);
       }
     }
-  }
+  };
 
   const login = async (username, password) => {
     // send request to server to login user
     try {
-      const response = await axios.post('/api/auth/login', { username, password });
+      const response = await axios.post('/api/auth/login', {
+        username,
+        password,
+      });
       if (response.status !== 200) {
         console.log('Failed to login:', response);
         // TODO - front-end: refresh page with 'invalid credentials' error message
         return;
       }
-      
+
       // success
       const data = response.data;
       if (data.username) {
@@ -121,37 +133,39 @@ export const AuthProvider = ({ children }) => {
           token: data.token,
         });
         console.log('login - setting username/token');
-        localStorage.setItem("username", data.username);
-        localStorage.setItem("token", response.headers['authorization']);
+        localStorage.setItem('username', data.username);
+        localStorage.setItem('token', response.headers['authorization']);
         // return navigate("/dashboard");
-        return;  // success
+        return; // success
       }
     } catch (err) {
       if (err.response) {
-          // The request was made and the server responded with a status code
-          // that falls out of the range of 2xx
-          console.log('Failed to login. Error response data:', err.response.data);
-          console.log('Failed to login. Error response status:', err.response.status);
-        } else if (err.request) {
-          // The request was made but no response was received
-          console.log('Error request:', err.request);
-        } else {
-          // Something happened in setting up the request that triggered an Error
-          console.log('Error message:', err.message);
-        }
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log('Failed to login. Error response data:', err.response.data);
+        console.log(
+          'Failed to login. Error response status:',
+          err.response.status
+        );
+      } else if (err.request) {
+        // The request was made but no response was received
+        console.log('Error request:', err.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log('Error message:', err.message);
+      }
     }
-    
   };
 
   const logout = () => {
     setAuthState({
       isAuth: false,
-      username: "",
-      userId: "",
+      username: '',
+      userId: '',
     });
-    localStorage.removeItem("username");
-    localStorage.removeItem("token");
-    return navigate("/");
+    localStorage.removeItem('username');
+    localStorage.removeItem('token');
+    return navigate('/');
   };
 
   return (
@@ -163,4 +177,4 @@ export const AuthProvider = ({ children }) => {
 
 export const useAuth = () => {
   return useContext(AuthContext);
-}
+};
