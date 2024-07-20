7492663b0677f35a239eab3d9adc5987102b9920
jibriyang91 <jibriyang91@gmail.com>
2024-07-20 12:04:23 -0400

    Auth Route Test Complete

---
+++ server/models/sequeliz.js
@@ -0,0 +1,9 @@
+// // db.js or sequelize.js
+// const { Sequelize } = require('sequelize');
+// const sequelize = new Sequelize(/* your database config */);
+
+// sequelize.authenticate()
+//     .then(() => console.log('Database connected...'))
+//     .catch(err => console.log('Error: ' + err));
+
+// module.exports = sequelize;
