b4ae02c1cff08a30fb271716fee52c021375b066
jibriyang91 <jibriyang91@gmail.com>
2024-07-20 12:22:34 -0400

    deleted sequlize file

--- server/models/sequeliz.js
+++
@@ -1,9 +0,0 @@
-// // db.js or sequelize.js
-// const { Sequelize } = require('sequelize');
-// const sequelize = new Sequelize(/* your database config */);
-
-// sequelize.authenticate()
-//     .then(() => console.log('Database connected...'))
-//     .catch(err => console.log('Error: ' + err));
-
-// module.exports = sequelize;
